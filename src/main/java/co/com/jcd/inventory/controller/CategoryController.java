8b64b8d26ec27e623f59f1dc1e366d5ab2654c4e
jcd <jrockb@gmail.com>
2024-02-19 12:19:38 -0500

    agregada configuracion de cors

--- src/main/java/co/com/jcd/inventory/controller/CategoryController.java
+++ src/main/java/co/com/jcd/inventory/controller/CategoryController.java
@@ -2,6 +2,7 @@ package co.com.jcd.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import co.com.jcd.inventory.request.CategoriaRequest;
 import co.com.jcd.inventory.response.CategoryResponseRest;
 import co.com.jcd.inventory.service.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
